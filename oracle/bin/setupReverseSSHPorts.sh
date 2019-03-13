efcd59fe36fc6e16e7df005b2b1feaeaf59ae30f
John Graves <john.graves@oracle.com>
2019-03-13 13:44:18 +1100

    fixed jump host

--- oracle/bin/setupReverseSSHPorts.sh
+++ oracle/bin/setupReverseSSHPorts.sh
@@ -9,5 +9,4 @@ echo "SSH PortNumber: ${sshPort}" >> /home/oracle/sshTunnel.log
 autosshBasePort="20${basePort}"
 autosshEchoPort="21${basePort}"
 
-#autossh -M ${autosshBasePort}:${autosshEchoPort} -q -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R [::]:${sshPort}:localhost:22 ports@streams.opcau.com >> /home/oracle/sshTunnel.log
-autossh -M ${autosshBasePort}:${autosshEchoPort} -q -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R [::]:${sshPort}:localhost:22 jump@132.145.45.69 >> /home/oracle/sshTunnel.log
+autossh -M ${autosshBasePort}:${autosshEchoPort} -q -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R [::]:${sshPort}:localhost:22 jump@jump.opcau.com >> /home/oracle/sshTunnel.log
